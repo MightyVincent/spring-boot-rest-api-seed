bbceab356c5a4d54fe6a1b29aa2c65aa733525f0
PC-20180417NK24\Administrator <ancienlee9@gmail.com>
2018-05-25 17:30:07 +0800

    Initial commit

---
+++ src/main/kotlin/com/company/project/SpringBootRestApiSeedApplication.kt
@@ -0,0 +1,11 @@
+package com.company.project
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootRestApiSeedApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootRestApiSeedApplication>(*args)
+}
