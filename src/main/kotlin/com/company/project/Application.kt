896fb409615640dac6568ea49efc360735036ff4
Vincent <ancienlee@live.com>
2018-05-28 12:41:11 +0800

    lots

--- src/main/kotlin/com/company/project/Application.kt
+++ src/main/kotlin/com/company/project/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class SpringBootRestApiSeedApplication
+class Application
 
 fun main(args: Array<String>) {
-    runApplication<SpringBootRestApiSeedApplication>(*args)
+    runApplication<Application>(*args)
 }
